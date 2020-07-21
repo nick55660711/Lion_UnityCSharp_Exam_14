0a702648fd4213c0e6f686aea43037b48e33f8fa
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-07-21 13:28:51 +0800

    14

--- FinalTest/Assets/C#/Q2.cs
+++ FinalTest/Assets/C#/Q2.cs
@@ -8,11 +8,27 @@ public class Q2 : MonoBehaviour
     public List<Sprite> AllPrize = new List<Sprite>();
     public Image Prize;
     public Text Message;
+    public Button Draw_B;
+    IEnumerator Change()
+        {
+        WaitForSecondsRealtime WAS = new WaitForSecondsRealtime(0.03f);
+
+        for (int i = 0; i < 22; i++)
+        {
+            Sprite tmp = AllPrize[Random.Range(0, 23)];
+            Prize.sprite = tmp;
+            yield return WAS;
+        }
+        Sprite tmp1 = AllPrize[Random.Range(0, 23)];
+            Prize.sprite = tmp1;
+        Message.text = "You got " + tmp1.name;
+        Draw_B.interactable = true;
+    }
 
     public void Draw() {
-        Sprite tmp = AllPrize[Random.Range(0, 23)];
-        Prize.sprite = tmp;
-        Message.text = "You got " + tmp.name;
+        Draw_B.interactable = false;
+        StartCoroutine(Change());
+       
 
     }
 }
